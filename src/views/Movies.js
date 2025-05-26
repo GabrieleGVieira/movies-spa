011e5a015e9ef659dd1073cb152661903973efb4
Gabriele Vieira <gabrielevieira011@gmail.com>
2025-05-25 21:22:38 -0300

    feat: add api call

--- src/views/Movies.js
+++ src/views/Movies.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react"
+import { getPopularMovies } from "../services/movies"
+
 export function Movies() {
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        getPopularMovies().then(({data}) => {
+            setMovies(data.results)
+        })
+    }, [])
+
     return (<>
-    <h1> Movies Page </h1>
+    <section>
+        <h1> Filmes Populares</h1>
+        <ul>
+            {movies.map((movie) => <li>{movie.title}</li>)}
+        </ul>
+    </section>
     </>
 )}

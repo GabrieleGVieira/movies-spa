0a0d1b346ffb302c71e186ab8d2fc093f32a6d45
Gabriele Vieira <gabrielevieira011@gmail.com>
2025-05-27 12:08:48 -0300

    feat: creating a field "react" for react project

--- src/config/http.js
+++
@@ -1,9 +0,0 @@
-import { default as axios } from "axios";
-
-export const moviesApis = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
-    headers: {
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5M2YyZThhM2NjMzQwM2EyZTdiMzRlMjZhZjVhYzZiZiIsIm5iZiI6MTc0ODIxNzE4NS4wMjgsInN1YiI6IjY4MzNhZDYxMDY3NTg4Yjg5N2E4NmUwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UkJJeWGhmqmRgRuqjunhhtLAnbefZw7OaJ4bNi37WvU"
-    }
-
-})
--- src/services/movies.service.js
+++
@@ -1,9 +0,0 @@
-import { moviesApis } from "../config/http";
-
-export function getPopularMovies() {
-    return moviesApis.get("movie/popular")
-}
-
-export function getMoviesDetails(movieId) {
-    return moviesApis.get(`movie/${movieId}`)
-}
